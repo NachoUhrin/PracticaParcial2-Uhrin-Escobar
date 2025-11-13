9c23a799c6e513b284a6f3ea752cd907d2b74665
NachoUhrin <nachouhrin@gmail.com>
2025-11-13 19:38:56 -0300

    Se creo la funcionalidad para mostrar las cards con los personajes principales en el inicio y sus clases

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
-const url = "https://hp-api.onrender.com/api/characters"
-.then((result) => result.json())
+const apiURL = "https://hp-api.onrender.com/api/characters";
+const container = document.getElementById("cards-container");
+
+
+async function cargarPersonajes() {
+  try {
+    const respuesta = await fetch(apiURL);
+    const personajes = await respuesta.json();
+
+    personajes.forEach(p => {
+
+      if (!p.image) return;
+
+      const card = document.createElement("div");
+      card.classList.add("card");
+
+      card.innerHTML = `
+        <img src="${p.image}" alt="${p.name}">
+        <h3>${p.name}</h3>
+        <p><strong>Año de nacimiento:</strong> ${p.yearOfBirth ?? "Desconocido"}</p>
+      `;
+
+
+      container.appendChild(card);
+    });
+
+  } catch (error) {
+    console.error("Error al cargar personajes:", error);
+    container.innerHTML = "<p>Error al cargar personajes.</p>";
+  }
+}
+
+cargarPersonajes();
+
